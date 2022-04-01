4af2d0741915f6fdd463780222b873439c511946
wuyadong <1002841782@qq.com>
2022-04-01 21:33:30 +0800

    casanova-sort

--- quickSort/my_quick.go
+++ quickSort/my_quick.go
@@ -3,24 +3,29 @@ package quickSort
 import "fmt"
 
 func QuickSort2(arr []int) []int {
-	if len(arr)<=1{
+	if len(arr) <= 1 {
 		return arr
 	}
-	head,tail:=0,len(arr)-1
-	mid:=arr[0]
-	for ;head<tail;{
-		for ;mid<arr[tail]&&head<tail;{
+	head, tail := 0, len(arr)-1
+	mid := arr[0]
+	for head < tail {
+		for mid < arr[tail] && head < tail {
 			tail--
 		}
-		arr[head]=arr[tail]
-		head++
-		for ;mid>arr[head];{
+		if head < tail {
+			arr[head] = arr[tail]
 			head++
 		}
-		arr[]
 
+		for mid > arr[head] && head < tail {
+			head++
+		}
+		if head < tail {
+			arr[tail] = arr[head]
+			tail--
+		}
 	}
-	arr[head]=mid
+	arr[head] = mid
 	fmt.Println(arr)
 	QuickSort2(arr[:head])
 	QuickSort2(arr[head+1:])
